2c03b40ef8b4c694fab9da4928a82141d403770f
Felipe Giacomelli <felipe.g@sinmec.ufsc.br>
2019-06-12 15:27:23 -0300

    Deleted Grid/

--- Main.py
+++ Main.py
@@ -5,77 +5,64 @@ import sys, os
 sys.path.append("/home/felipe/SINMEC2018/PythonTools/Felipe/CgnsFile")
 from CgnsFile import CgnsFile
 
-sys.path.append("Grid/")
-from Vertex import Vertex
-from Element import Element
-
 def main():
     cgnsFile = CgnsFile("/home/felipe/Downloads/12v_6x_2y.cgns")
 
     X = cgnsFile.coordinateX
     Y = cgnsFile.coordinateY
-    nX = X.shape[1]
-    nY = X.shape[0]
-    cells = np.zeros((nY-1, nX-1), dtype=int)
+
+    NVertexI = cgnsFile.zone.get(" data")[()][0][0]
+    NVertexJ = cgnsFile.zone.get(" data")[()][0][1]
+    NCellI = cgnsFile.zone.get(" data")[()][1][0]
+    NCellJ = cgnsFile.zone.get(" data")[()][1][1]
+
+    cells = np.zeros((NCellJ, NCellI), dtype=int)
 
     K = 1.0
     C = 1.0
 
-    for j in range(0, nY-1):
-        for i in range(0, nX-1):
-            cells[j][i] = j*nX + i
+    for j in range(0, NCellJ):
+        for i in range(0, NCellI):
+            cells[j][i] = j*NVertexI + i
 
     ###########################
 
-    vertices = []
-    for j in range(0, nY):
-        for i in range(0, nX):
-            vertices.append(Vertex(X[j][i], Y[j][i], j*nX + i))
-
-    elements = []
-    for j in range(0, nY - 1):
-        for i in range(0, nX - 1):
-            elements.append(Element(X[j][i], X[j][i+1], Y[j][i], Y[j+1][i], j*nX + i))
-
-    ###########################
-
-    west = np.zeros(nX)
-    for i in range(1, nX):
+    west = np.zeros(NVertexI)
+    for i in range(1, NVertexI):
         west[i] = X[0][i] - X[0][i-1]
     west[0] = 0.5 * (X[0][1] - X[0][0])
 
-    east = np.zeros(nX)
-    for i in range(0, nX-1):
+    east = np.zeros(NVertexI)
+    for i in range(0, NVertexI-1):
         east[i] = X[0][i+1] - X[0][i]
     east[-1] = 0.5 * (X[0][-1] - X[0][-2])
 
-
     ###########################
 
-    A = np.zeros(((nX-1) * (nY-1), (nX-1) * (nY-1)))
+    A = np.zeros((NCellI * NCellJ, NCellI * NCellJ))
 
-    for j in range(0, nY - 1):
-        for i in range(1, nX - 2):
+    for j in range(0, NCellJ):
+        for i in range(1, NCellI - 1):
             A[cells[j][i]][cells[j][i-1]] = -1.0 * K / (C * west[i])
             A[cells[j][i]][cells[j][i]]   = K / (C * west[i]) + K / (C * east[i])
             A[cells[j][i]][cells[j][i+1]] = -1.0 * K / (C * east[i])
 
-    for j in range(0, nY - 1):
+    for j in range(0, NCellJ):
         A[cells[j][0]][cells[j][0]] = K / (C * west[0]) + K / (C * east[0])
         A[cells[j][0]][cells[j][1]] = -1.0 * K / (C * east[0])
 
-    for j in range(0, nY - 1):
+    for j in range(0, NCellJ):
         A[cells[j][-1]][cells[j][-2]] = -1.0 * K / (C * west[-1])
         A[cells[j][-1]][cells[j][-1]] = K / (C * west[-1]) + K / (C * east[-1])
 
-    b = np.zeros((nX-1) * (nY-1))
+    b = np.zeros(NCellI * NCellJ)
     b[0] = 20.0 * K / (C * west[0])
     b[-1] = 100.0 *  K / (C * east[-1])
 
     t = np.linalg.solve(A, b)
 
-    x = np.zeros((nX-1) * (nY-1))
-    for i in range(0, nX - 1):
+    x = np.zeros(NCellI * NCellJ)
+    for i in range(0, NCellI):
         x[i] = 0.5 * (X[0][i+1] + X[0][i])
 
     T = 80.0*X[0] + 20.0
@@ -99,27 +86,19 @@ def main():
 
     ###########################
 
-    # print("\n\tvertices")
-    # for vertex in vertices:
-    #     vertex.print()
-
-    # print("\n\telements")
-    # for element in elements:
-    #     element.print()
-
-    print("\n\tX - %i" % nX)
-    for i in range(0, nX):
+    print("\n\tX - %i" % NVertexI)
+    for i in range(0, NVertexI):
         print("\t\t%.5f" % X[0][i])
 
-    print("\n\tY - %i" % nY)
-    for j in range(0, nY):
+    print("\n\tY - %i" % NVertexJ)
+    for j in range(0, NVertexJ):
         print("\t\t%.5f" % Y[j][0])
 
     print("\n\tcells")
-    for j in range(0, nY-1):
+    for j in range(0, NCellJ):
         print("\t\t", end="")
-        for i in range(0, nX-1):
-            print("%2i " % (j*nX + i), end="")
+        for i in range(0, NCellI):
+            print("%2i " % (j*NVertexI + i), end="")
     print()
 
     print("\n\tw")
